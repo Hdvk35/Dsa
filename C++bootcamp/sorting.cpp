71caffa6316028014331458206bd062a98637d15
hdvk35 <harshitdhumal@gmail.com>
2024-06-10 17:51:54 +0530

    bubble sort

--- C++bootcamp/sorting.cpp
+++ C++bootcamp/sorting.cpp
@@ -42,7 +42,7 @@ int main()
     int arr[] = { 64, 25, 12, 22, 11 };
     int n = sizeof(arr) / sizeof(arr[0]);
 
-        selectionSort(arr,n);
+        bubbleSort(arr,n);
      cout << "Sorted array: \n";
     printArray(arr, n);
     return 0;
