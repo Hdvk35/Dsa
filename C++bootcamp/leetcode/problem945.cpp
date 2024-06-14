89ccc76c0f98c493555442ce9e290d6641d3bd52
hdvk35 <harshitdhumal@gmail.com>
2024-06-14 17:41:03 +0530

    adding leetcode problem

---
+++ C++bootcamp/leetcode/problem945.cpp
@@ -0,0 +1,13 @@
+// int minIncrementForUnique(vector<int>& nums) {
+//         int ans=0;
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+//         for(int i=1; i<n; i++)
+//         {
+//             if(nums[i]<=nums[i-1]){
+//                 ans = ans + (nums[i-1]-nums[i]) +1;
+//                 nums[i] = nums[i-1]+1;
+//             }
+//         }
+//         return ans;
+//     }
